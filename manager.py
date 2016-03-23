0cbfcb4140d3ab86375caded69b0ca2ac92061e5
马金凤 <1021008546@qq.com>
2016-03-23 10:30:14 +0800

    多线程共用一个线程函数，线程运行平均的脚本数

--- manager.py
+++ manager.py
@@ -48,7 +48,8 @@ class Run():
         self.test_cases = []
         self.test_case_error = 0
         self.test_case_success = 0
-        self.run_time_sum = 0
+        self.no_thread_run_time_sum = 0
+        self.thread_all_run_time_sum = 0
 
     def data(self):
         for root, dirs, files in os.walk(self.root):
@@ -71,37 +72,47 @@ class Run():
             print test_case.root, test_case.file, test_case.run_time, test_case.result
         print 'test_case_success : %d \n' % self.test_case_success
         print 'test_case_error : %d \n' % self.test_case_error
-        print 'run_time_sum : %.8f sec' % self.run_time_sum
+        print 'no_thread_run_time_sum : %.8f sec' % self.no_thread_run_time_sum
+        print 'thread_all_run_time_sum : %.8f sec' % self.thread_all_run_time_sum
 
-    def thread1(self):
-        for thread1_test_case in self.test_cases[: 2]:
+    def thread_all(self, test_cases):
+        for thread1_test_case in test_cases:
             time_s = time.time()
             result = os.popen('python  %s' % thread1_test_case.file)
             thread1_test_case.result = result.read()
             time_e = time.time()
             thread1_test_case.run_time = time_e-time_s
-            print "thread1_test_case : %s" % thread1_test_case.result
+            print "thread_all_test_case : %s" % thread1_test_case.result
 
-    def thread2(self):
-        for thread2_test_case in self.test_cases[2: ]:
+    def nothread(self):
+        for test_case in self.test_cases:
             time_s = time.time()
-            thread2_test_case.result = os.popen('python  %s' % thread2_test_case.file).read()
+            test_case.result = os.popen('python  %s' % test_case.file).read()
             time_e = time.time()
-            thread2_test_case.run_time = time_e-time_s
-            print "thread2_test_case : %s" % thread2_test_case.result
+            test_case.run_time = time_e-time_s
+            print "no_thread_test_case : %s" % test_case.result
 
     def run_test_case(self):
         self.data()
         time_start = time.time()
-        thread1 = threading.Thread(target=self.thread1)
+        thread1 = threading.Thread(target=self.thread_all, args=(self.test_cases[: len(self.test_cases)/2],))
         thread1.start()
-        thread2 = threading.Thread(target=self.thread2)
+        thread2 = threading.Thread(target=self.thread_all, args=(self.test_cases[len(self.test_cases)/2:],))
         thread2.start()
         thread1.join()
         thread2.join()
         time_end = time.time()
-        self.run_time_sum = time_end-time_start
+        self.thread_all_run_time_sum = time_end-time_start
         self.runlog()
+
+        time_start1 = time.time()
+        self.nothread()
+        time_end1 = time.time()
+        self.no_thread_run_time_sum = time_end1-time_start1
+        self.runlog()
+
+
+
 
 
 test = Run()
